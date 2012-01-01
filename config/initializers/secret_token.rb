27b1091d262b7f718bdc296b22949b92b6a9b60a
Pavel Shved <pavel.shved@gmail.com>
2012-01-02 03:57:37 +0600

    Deploy rails3 with a couple of options

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zlo::Application.config.secret_token = '09c44f06cc90b40868826813fad46afadbdb85234667cc16cdf2c8d3ff58d0f91128e72f28adfa875b00f4413fe7482d67fbb4f20a8a373a24db22bcc84a2d52'
