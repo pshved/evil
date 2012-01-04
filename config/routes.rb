3e296c8915d444c680384a72053d6ac79274edeb
Pavel Shved <pavel.shved@gmail.com>
2012-01-04 15:46:54 +0600

    Make users authentic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Zlo::Application.routes.draw do
+  root :controller => 'users', :action => 'index'
+
   resources :users
 
   # The priority is based upon order of creation:
@@ -47,10 +49,6 @@ Zlo::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
