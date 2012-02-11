78277a8b6b6c05f44eb65e7570028860c74bfd15
Pavel Shved <pavel.shved@gmail.com>
2012-02-11 18:05:30 +0400

    Temporarily remove unique constraint from index
    As the datbase is already corrupted in production.  See issue #39.

--- db/migrate/20120211130029_add_keys_to_hidden_posts.rb
+++ db/migrate/20120211130029_add_keys_to_hidden_posts.rb
@@ -2,6 +2,6 @@ class AddKeysToHiddenPosts < ActiveRecord::Migration
   def change
     add_index :hidden_posts_users, :user_id
     add_index :hidden_posts_users, :posts_id
-    add_index :hidden_posts_users, [:user_id,:posts_id], :unique => true
+    add_index :hidden_posts_users, [:user_id,:posts_id]
   end
 end
