f94987a0ebfb8aed685d2d7b838d85a8672e13bc
Pavel Shved <pavel.shved@gmail.com>
2012-03-04 19:44:34 +0400

    Add memcached basic configuration
    It uses Dalli gem.  Replaced default cache for Rails stuff with memcache
    storage.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Zlo::Application.initialize!
+
+# Passenger and memcached integration (see https://github.com/mperham/dalli)
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # Reset Rails's object cache
+    # Only works with DalliStore
+    Rails.cache.reset if forked
+  end
+end
