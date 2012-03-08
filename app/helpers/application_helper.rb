40cf94c4129f0eba7f8ed06c4e3283e56b32d40e
Pavel Shved <pavel.shved@gmail.com>
2012-03-09 01:49:55 +0400

    Fix refactored user_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,8 +8,16 @@ module ApplicationHelper
   end
 
   # Helper to create nice user links in a fast manner
-  def user_link(user)
-    link_to user.login, user_path(user)
+  def user_link(user, maybe_unreg = nil)
+    if user
+      if user == current_user
+        haml_tag 'span.post-self',link_to(user.login, user_path(user))
+      else
+        haml_tag 'span.post-user',link_to(user.login, user_path(user))
+      end
+    else
+      haml_tag 'span.post-unreg',(maybe_unreg || 'NIL')
+    end
   end
 
   # Nice time conversion
