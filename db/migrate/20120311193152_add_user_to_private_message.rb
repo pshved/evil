01eab06b581031308ff1d511e7ac180dc8e8130e
Pavel Shved <pavel.shved@gmail.com>
2012-03-12 21:47:54 +0400

    Merge two PM migrations

--- db/migrate/20120311193152_add_user_to_private_message.rb
+++ db/migrate/20120311193152_add_user_to_private_message.rb
@@ -2,8 +2,11 @@ class AddUserToPrivateMessage < ActiveRecord::Migration
   def change
     add_column :private_messages, :user_id, :integer
     add_index :private_messages, :user_id
+    add_column :private_messages, :unread, :boolean, :default => false
 
     # Duplicate all messagea
     PrivateMessage.all.each {|pm| pm.create_duplicate}
+
+    PrivateMessage.update_all :unread => false
   end
 end
--- db/migrate/20120311211646_add_unread_to_private_message.rb
+++ db/migrate/20120311211646_add_unread_to_private_message.rb
@@ -1,7 +1,4 @@
 class AddUnreadToPrivateMessage < ActiveRecord::Migration
   def change
-    add_column :private_messages, :unread, :boolean, :default => false
-
-    PrivateMessage.update_all :unread => false
   end
 end
