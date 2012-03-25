0e441bdb84a63498a87219d00feddb79e27ef780
Pavel Shved <pavel.shved@gmail.com>
2012-03-26 01:39:57 +0400

    Increase click precise threshold
    Since we cache index pages more aggressively, we may add some here.

--- config/initializers/perf_randoms.rb
+++ config/initializers/perf_randoms.rb
@@ -1,5 +1,5 @@
 # If we update each thread at each click, we'll lose the ability to cache them.
 # Instead, we drop precision in sake of performance.
 # When a post has less than CLICK_UPDATE_THRESHOLD clicks, the thread is always updated.  Otherwise, the thread is updates once per CLICK_DELAY_RATE clicks (as decided by random number generator)
-CLICK_UPDATE_THRESHOLD = 2
+CLICK_UPDATE_THRESHOLD = 3
 CLICK_DELAY_RATE = 5
