9f312d143c0e5a352753dc9e2f18acb4b46ca092
Pavel Shved <pavel.shved@gmail.com>
2012-03-25 20:29:26 +0400

    Do not try to find presentation if cookies are empty
    This should save a DB call.

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -54,6 +54,8 @@ class Presentation < ActiveRecord::Base
   # Loads the presentation from cookies
   def self.from_cookies(cookies)
     key = cookies[:presentation_key]
+    # If nothing is recorded in cookies, then there's no presentation
+    return nil unless key
     # Global presentation has cookie_key and user_id equal to nil, but global is "true" for it.
     if r = Presentation.find_last_by_cookie_key_and_user_id_and_global(key,nil,false)
       # Technically, we need to update access time each time we load the presentation.  This would exhibit high load on the server.
@@ -64,7 +66,7 @@ class Presentation < ActiveRecord::Base
       end
       r
     else
-      # If there's no presentation in cookies, return nothing.  We'll get to the default presentation via controller's current_presentation
+      # Cookies don't contain a valid presentation index, return nothing.  We'll get to the default presentation via controller's current_presentation
       nil
     end
   end
