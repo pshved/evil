7a471c6185dc9bb1e7229e1248faeeb0da474f7c
Pavel Shved <pavel.shved@gmail.com>
2012-06-04 00:10:21 +0400

    Add iframe with a form to post to the original board
    The form resides in an iframe, and its reply is NOT processed.  It uses
    a hardcoded cp1251 encoding for the form.

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,12 +1,28 @@
+require 'iconv'
 class SourcesController < ApplicationController
   before_filter :find_source_by_id
+  before_filter :find_import_by_post_id
   def read_post
-    @post = @source.imports.where(:back => params[:orig_id]).first.post
+    @post = @import.post
     redirect_to @post
+  end
+
+  after_filter :force_reply_encoding, :only => :iframe
+  def iframe
+    @source_reply_to = params[:orig_id]
+    render :file => 'posts/sourcepost_iframe', :layout => false
   end
 
   protected
   def find_source_by_id
     @source = Source.find_last_by_name(params[:id])
+  end
+  def find_import_by_post_id
+    @import = @source.imports.where(:back => params[:orig_id]).first
+  end
+
+  def force_reply_encoding
+    response.charset = 'cp1251'
+    response.body = Iconv.conv('cp1251//IGNORE//TRANSLIT','UTF-8',response.body)
   end
 end
