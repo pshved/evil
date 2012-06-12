cb082268e55f7a65ceb35edf873a800b139f981b
Pavel Shved <pavel.shved@gmail.com>
2012-06-12 21:35:51 +0400

    Add foreign post redirect that violates Same Origin Policy
    This doesn't work if the source forum is on the other domain.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,5 +8,23 @@
 //= require jquery_ujs
 //= require_tree .
 
-
-
+function waitForSourcePost(iframe_name,source_name)
+{
+  var iframe_selector = 'iframe[name='+iframe_name+']'
+  // At each frame load, we check if the frame designates a successful post.  If it does, then wait for it to be imported, ad redirect.
+  $(iframe_selector).load(function (e) {
+    href_elem = $(this).contents().find('p b a').first();
+    if (href_elem.length > 0){
+      var post_href = href_elem.attr('href');
+      var post_regexp = /\?read=/;
+      if (post_regexp.test(post_href)){
+        // There is a link that follows to a post at the source forum, get the post id
+        var post_id = post_href.replace(post_regexp,'');
+        // Notify the source that it should notify the downloader
+        $.get('/sources/'+source_name+'/instant');
+        // Redirect the post as soon as it's imported (TODO + ajax)
+        setTimeout(function(){window.location.replace("/sources/"+source_name+"/read/"+post_id);},5000);
+      }
+    }
+  });
+}
