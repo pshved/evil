55eb5f126a5c9d6ecb5cf8e58577264016b7134c
Pavel Shved <pavel.shved@gmail.com>
2012-06-14 00:34:53 +0400

    Do not validate title when importing posts
    It would make the forum automatically import posts with too long titles
    (for instance, due to HTML markup).

--- lib/autoload/post_validations.rb
+++ lib/autoload/post_validations.rb
@@ -1,7 +1,7 @@
 module PostValidators
   # Include all validations here
   def validates_post_attrs
-    validates_length_of :title, :maximum => 100
+    validates_length_of :title, :maximum => 100, :if => proc {|p| p.text_container.filter == :board }
     validates_presence_of :title
     # Prevent uploading tons of crap
     validates_length_of :body, :maximum => 1_000_000
